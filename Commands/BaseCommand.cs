a340440a5e6a2f5e9339621f1e8b45ab2e87b885
whojinn <github@mail.whojinn.com>
2022-11-18 00:57:23 +0900

    Add string constant _rootDescription

--- Commands/BaseCommand.cs
+++ Commands/BaseCommand.cs
@@ -5,9 +5,11 @@ using System.Threading.Tasks;
 
 internal class BaseCommand
 {
+    private const string _rootDescripton = "Client for ConoHa VPS.";
+
     public async Task<int> Execute(string[] args)
     {
-        var rootCommand = new RootCommand("Client for ConoHa VPS.");
+        var rootCommand = new RootCommand(_rootDescripton);
 
         rootCommand.SetHandler(() =>
         {
